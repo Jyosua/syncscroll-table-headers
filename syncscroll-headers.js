49d17e7083480a93176bf8e4aebc8b1cb67d1b59
Jyosua <joshua.moore@jyosua.net>
2021-05-22 21:45:25 +0900

    Fixed to duplicate headers and sync them.

--- syncscroll-headers.js
+++ syncscroll-headers.js
@@ -10,34 +10,72 @@
 		factory( jQuery );
 	}
 }(function( $ ) {
-var elementsByClass = {};
-$('.syncscroll,[class^=sync-name-]').each(function (i,thing) {
-    let clsName = $(this).attr('class');
-    let length = (clsName in elementsByClass ? elementsByClass[clsName].length : 0);
-    if(length === 0)
-        elementsByClass[clsName] = [];
-    elementsByClass[clsName][length] = $(this);
-})
+    var scrollableDivsByClass = {};
+    setSyncElements();
+    createStickyHeaders();
+    resetSyncElements();
 
-$(window).on('resize',function() {
-    Object.keys(elementsByClass).forEach(key => {
-        if (elementsByClass[key].length != 2) // Ignore a potential usecase other than tables
-            return;
+    $(window).on('resize', function () {
+        syncHeaders();
+    });
+    $(document).ready(function () {
+        syncHeaders();
+    });
 
-        let headerDiv = elementsByClass[key][0];
-        let tableDiv = elementsByClass[key][1];
+    function resetSyncElements() {
+        clearSyncElements();
+        setSyncElements();
+    }
 
-        // Set table widths
-        headerDiv.find('table').eq(0).width(tableDiv.find('table').eq(0).width());
-
-        // Resize header tags to match
-        let headers = headerDiv.find('th');
-        tableDiv.find('tr').eq(0).children().each(function (index, cell) {
-            headers.eq(index).width($(this).width())
+    function clearSyncElements() {
+        scrollableDivsByClass = {};
+    }
+    
+    function setSyncElements() {
+        $('[class*="syncscroll sync-name-"]').each(function (i, thing) {
+            var fullClassName = $(this).attr('class');
+            var classInstancesArrayLength = (fullClassName in scrollableDivsByClass ? scrollableDivsByClass[fullClassName].length : 0);
+            if (classInstancesArrayLength === 0)
+                scrollableDivsByClass[fullClassName] = [];
+            scrollableDivsByClass[fullClassName][classInstancesArrayLength] = $(this);
         });
-      });
-});
-$(document).ready(function() {
-    $(window).trigger('resize');
-});
+    }
+
+    function createStickyHeaders() {
+        Object.keys(scrollableDivsByClass).forEach(function(key) {
+            if (scrollableDivsByClass[key].length != 1) // Ignore duplicated keys
+                    return;
+            
+            var elem = scrollableDivsByClass[key][0];
+            if(elem.parent(".table").length === 1) {
+                var headersClones = elem.find('tr').eq(0).clone()
+                var parentDiv = elem.parent(".table").eq(0).prepend("<div class=\""+key+"\" style=\"overflow-x: hidden; position: sticky; top: 50px;\"><table><tbody></tbody></table></div>")
+                parentDiv.find('tbody').eq(0).prepend(headersClones)
+            }
+        });
+    }
+
+    function syncHeaders() {
+        Object.keys(scrollableDivsByClass).forEach(function(key) {
+            if (scrollableDivsByClass[key].length != 2) // Ignore a potential usecase other than tables
+                return;
+
+            var syncscrollTableHeaderDiv = scrollableDivsByClass[key][0];
+            var syncscrollTableDiv = scrollableDivsByClass[key][1];
+
+            // Set table widths
+            syncscrollTableHeaderDiv.find('table').eq(0).width(syncscrollTableDiv.find('table').eq(0).width());
+
+            // Resize header tags to match
+            var syncscrollTableHeaders = syncscrollTableHeaderDiv.find('th');
+            syncscrollTableDiv.find('tr').eq(0).children().each(function (index, cell) {
+                syncscrollTableHeaders.eq(index).width($(this).width())
+            });
+
+            // Setup scroll events for syncing scroll positions
+            syncscrollTableDiv.on('scroll', function() {
+                syncscrollTableHeaderDiv.scrollLeft($(this).scrollLeft());
+            }); 
+        });
+    }
 }));
